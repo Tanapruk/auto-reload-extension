b71b72fef6f4f30321cb0b2118f61aee7e604fea
Tanapruk <trusthaha@gmail.com>
2017-02-19 13:48:26 +0700

    first commit

---
+++ autoreload.js
@@ -0,0 +1,11 @@
+
+//create new element
+var newdiv = document.createElement("DIV");
+newdiv.innerHTML = "some tesss"
+document.body.appendChild(newdiv);
+
+//reload
+setTimeout(function() {
+	window.location.href = 'https://www.lotto.ktbnetbank.com/';
+
+}, 10000)
